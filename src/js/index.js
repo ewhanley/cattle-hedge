c508297a0d2571e22763b9a85679dcb837ccfdf7
Eric Hanley <ewhanley@gmail.com>
2018-03-17 23:00:35 -0700

    Binds input values

--- src/js/index.js
+++ src/js/index.js
@@ -12,10 +12,15 @@ document.querySelector('.toggle-button').addEventListener('click', function () {
 });
 
 let ViewModel = function () {
+  let today = moment();
   let self = this;
   self.marketWt = ko.observable();
   self.ADG = ko.observable();
-  self.buyDate = ko.observable();
+  self.buyDate = ko.observable(today);
+
+  self.fmtBuyDate = ko.computed(function () {
+    return moment(self.buyDate()).format('L');
+  });
 }
 
 ko.applyBindings(new ViewModel());
